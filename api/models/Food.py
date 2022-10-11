eb88d70a38a13e976dc0754790d6e057e034b5e1
Femi Olatubosun <thefemiolatubosun@gmail.com>
2022-10-11 10:43:18 +0100

    Updated Food model

--- api/models/Food.py
+++ api/models/Food.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class Food(models.Model):
-    class Food(models.Model):
-        name = models.CharField(max_length=255, unique=True)
-        price = models.FloatField(default=0)
-        description = models.TextField()
-        # photo
+    name = models.CharField(max_length=255, unique=True)
+    price = models.FloatField(default=0)
+    description = models.TextField()
+    # photo
